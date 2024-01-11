cd1cfbe6cbe6b91d6ada3e9243777cf23a599e35
Zenia Dippenaar <zeniadippenaar@gmail.com>
2024-01-11 11:10:09 +0200

    Setup portfolio slice and reduce api calls to coin data

--- components/PortfolioItem/index.tsx
+++ components/PortfolioItem/index.tsx
@@ -7,8 +7,8 @@ const PortfolioItem = ({portfolio, portfolioCoins, currency} : {portfolio: Portf
   const coinData: Coin = portfolioCoins.find((item) => item.id === portfolio.id)!;
 
   return (
-    <div className="mb-4 bg-blackberry flex min-h-[200px] rounded-xl">
-      <div className="w-1/3 bg-[#191932] rounded-l-xl py-4 pl-4 pr-2">
+    <div className="mb-4 dark:bg-blackberry bg-white flex min-h-[200px] rounded-xl">
+      <div className="w-1/3 dark:bg-[#191932] bg-[#6161de80] rounded-l-xl py-4 pl-4 pr-2">
         <div className="flex mb-6 items-center pl-1">
           <Image src={coinData.image} alt={coinData.name} width={40} height={40}/>
           <h2 className="capitalize text-xl ml-2">{coinData.name} (<span className="uppercase">{coinData.symbol}</span>)</h2>
